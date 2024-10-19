323d775cc10cb577d291f2ad5628039b4a23fd02
Maria Zharova <57617173+MariaZharova@users.noreply.github.com>
2024-10-19 22:16:26 +0300

    Add files via upload

---
+++ MFTIDS/ML_in_prod/4/client_1.py
@@ -0,0 +1,16 @@
+import requests
+
+if __name__ == '__main__':
+    # выполняем POST-запрос на сервер по эндпоинту predict с параметром json
+    r = requests.post('http://localhost:5000/predict', json=[10, 10, 10])
+    # выводим статус запроса
+    print('Status code: {}'.format(r.status_code))
+    # реализуем обработку результата
+    if r.status_code == 200:
+        # если запрос выполнен успешно (код обработки=200),
+        # выводим результат на экран
+        print('Prediction: {}'.format(r.json()['prediction'])) 
+    else: 
+        # если запрос завершён с кодом, отличным от 200, 
+        # выводим содержимое ответа 
+        print(r.text) 
